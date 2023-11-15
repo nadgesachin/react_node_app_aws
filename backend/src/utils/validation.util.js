7daea611ed544224be852cd5762e3534f59a2017
Errorpat-404 <nadgesachin644@gmail.com>
2023-11-15 19:02:20 +0530

    Revert "initial commit"
    This reverts commit b4efe9b61a864ffb03cbb925638bbb6e7b06ca84.

--- backend/src/utils/validation.util.js
+++
@@ -1,8 +0,0 @@
-export const validateEmail = (email) => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailRegex.test(email);
-  };
-
-  export const validatePhoneNumber=(phoneNumber)=>{
-    return /^[0-9]{10}$/.test(phoneNumber);
-  }
